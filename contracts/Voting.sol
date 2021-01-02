4b1bd75f87b3d80e4746abc74cc5a7b94ebe86aa
Oladiy <bd3396121@gmail.com>
2021-01-02 17:45:32 +0300

    [ADD] Constructor, bidding end, reveal end, scale max value, beneficiary name and address

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,4 +1,26 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
+    address payable public beneficiary;
+
+    uint public biddingEnd;
+    uint public revealEnd;
+    /// Максимально возможное значение голоса
+    uint public scaleMaxValue;
+
+    string public beneficiaryName;
+
+    constructor(
+        uint _biddingTime,
+        uint _revealTime,
+        uint _scaleMaxValue,
+        address payable _beneficiary,
+        string memory _beneficiaryName
+    ) {
+        biddingEnd = block.timestamp + _biddingTime;
+        revealEnd = biddingEnd + _revealTime;
+        scaleMaxValue = _scaleMaxValue;
+        beneficiary = _beneficiary;
+        beneficiaryName = _beneficiaryName;
+    }
 }
