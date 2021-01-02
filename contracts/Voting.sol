22eebfefc937c35aaaef2114f0d3071ad11562e3
Oladiy <bd3396121@gmail.com>
2021-01-02 19:48:16 +0300

    [ADD] vote and some other variables for that

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -4,6 +4,12 @@ contract Voting {
     /// Адрес того, за кого голосуют
     address payable public beneficiary;
 
+    /// Таблица тех, кто голосовал
+    mapping(address => bool) public voters;
+
+    /// Таблица голос + депозит
+    mapping(address => Vote) public votes;
+
     /// Окончание возможности сделать голос
     uint public votingEnd;
     /// Окончание возможности раскрытия
@@ -11,11 +17,21 @@ contract Voting {
     /// Максимально возможное значение голоса
     uint public scaleMaxValue;
 
+    struct Vote {
+        string vote;
+        uint deposit;
+    }
+
     /// Имя того, за кого голосуют
     string public beneficiaryName;
 
     modifier checkBalance() {
         require(msg.sender.balance >= scaleMaxValue);
+        _;
+    }
+
+    modifier checkDidNotVote() {
+        require(!voters[msg.sender]);
         _;
     }
 
@@ -41,5 +57,40 @@ contract Voting {
         scaleMaxValue = _scaleMaxValue;
         beneficiary = _beneficiary;
         beneficiaryName = _beneficiaryName;
+    }
+
+    function vote(
+        string memory _vote // возможно нужно bytes32 - подумать!
+    )
+    public
+    payable
+    onlyBefore(votingEnd)
+    checkBalance()
+    checkDidNotVote()
+    {
+        votes[msg.sender] = Vote({
+            vote: _vote,
+            deposit: msg.value
+        });
+        voters[msg.sender] = true;
+    }
+
+    function reveal(
+
+    )
+    public
+    onlyAfter(votingEnd)
+    onlyBefore(revealEnd)
+    {
+
+    }
+
+    function endVoting(
+
+    )
+    public
+    onlyAfter(revealEnd)
+    {
+
     }
 }
