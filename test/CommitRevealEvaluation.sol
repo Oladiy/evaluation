98528b31983be96db058baa978439206d2a2798d
Oladiy <bd3396121@gmail.com>
2021-01-05 14:27:50 +0300

    [ADD] Check juries and fake juries in list after deploying

--- test/CommitRevealEvaluation.sol
+++ test/CommitRevealEvaluation.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.7.0;
 
 import "truffle/AssertAddress.sol";
+import "truffle/AssertBool.sol";
 import "truffle/AssertString.sol";
 import "truffle/AssertUint.sol";
 import "truffle/DeployedAddresses.sol";
@@ -19,5 +20,14 @@ contract TestVoting {
         AssertUint.equal(evaluation.scaleMaxValue(), expectedScaleMaxValue, "Compare scale max value after deploying contract");
         AssertAddress.equal(evaluation.beneficiary(), expectedBeneficiary, "Compare beneficiary address after deploying contract");
         AssertString.equal(evaluation.beneficiaryName(), expectedBeneficiaryName, "Compare beneficiary name after deploying contract");
+
+        address firstJury = 0xD0D1597614662cf53C1ADA223D9268b984B68714;
+        address secondJury = 0x2c61097258C54cE52143224a5169cA082A6c7203;
+        address thirdJury = 0x5C9b7ce8b884f5D988578d3B58DcBCF8Fa15F758;
+        address fakeJury = 0x67fDea9dFACc29a20aef5Cd0B833B7d0485AeDfd;
+        AssertBool.equal(evaluation.juries(firstJury), true, "Check if jury address is marked as true after deploying contract");
+        AssertBool.equal(evaluation.juries(secondJury), true, "Check if jury address is marked as true after deploying contract");
+        AssertBool.equal(evaluation.juries(thirdJury), true, "Check if jury address is marked as true after deploying contract");
+        AssertBool.equal(evaluation.juries(fakeJury), false, "Check if fake jury address is marked as false after deploying contract");
     }
 }
