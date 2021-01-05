1d12aabfd9894b1adf006583a39394c1d7c8e4ea
Oladiy <bd3396121@gmail.com>
2021-01-05 14:27:25 +0300

    [ADD] Juries addresses when deploying

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const CommitRevealEvaluation = artifacts.require("CommitRevealEvaluation");
 
 module.exports = function (deployer) {
-    deployer.deploy(CommitRevealEvaluation, 10, 20, 10, "0x10E1Be9F6A5aD767b6fbB9ab32cEA8cf208f1543", "Anthony Zuc");
+    deployer.deploy(CommitRevealEvaluation,
+        10, 20, 10,
+        "0x10E1Be9F6A5aD767b6fbB9ab32cEA8cf208f1543",
+        "Anthony Zuc",
+        [
+            "0xD0D1597614662cf53C1ADA223D9268b984B68714",
+            "0x2c61097258C54cE52143224a5169cA082A6c7203",
+            "0x5C9b7ce8b884f5D988578d3B58DcBCF8Fa15F758",
+        ]);
 };
