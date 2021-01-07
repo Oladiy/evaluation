f161eac237ac025c44efe126791d35db44f9a3a5
Oladiy <bd3396121@gmail.com>
2021-01-07 11:52:42 +0300

    [UPDATE] Some default values

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const ABDKMathQuad = artifacts.require("ABDKMathQuad");
 
 module.exports = function (deployer) {
     deployer.deploy(CommitRevealEvaluation,
-        10, 20, 10,
+        15, 30, 10,
         "0x10E1Be9F6A5aD767b6fbB9ab32cEA8cf208f1543",
         "Anthony Zuc",
         [
