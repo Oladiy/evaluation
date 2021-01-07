32d982d83462a86dda8e59eb2dccf783666604e4
Oladiy <bd3396121@gmail.com>
2021-01-07 11:43:32 +0300

    [ADD] tests for reveal and revealEnd functions. Jury and beneficiary addresses are placed outside of functions as contract states

--- test/CommitRevealEvaluation.sol
+++ test/CommitRevealEvaluation.sol
@@ -12,18 +12,20 @@ contract TestEvaluation {
         DeployedAddresses.CommitRevealEvaluation()
     );
 
+
+    address firstJury = 0xD0D1597614662cf53C1ADA223D9268b984B68714;
+    address secondJury = 0x2c61097258C54cE52143224a5169cA082A6c7203;
+    address thirdJury = 0x5C9b7ce8b884f5D988578d3B58DcBCF8Fa15F758;
+    address beneficiary = 0x10E1Be9F6A5aD767b6fbB9ab32cEA8cf208f1543;
+
     function testGetContractStatesAfterDeploying() public {
         uint expectedScaleMaxValue = 10;
-        address expectedBeneficiary = 0x10E1Be9F6A5aD767b6fbB9ab32cEA8cf208f1543;
         string memory expectedBeneficiaryName = "Anthony Zuc";
 
         AssertUint.equal(evaluation.scaleMaxValue(), expectedScaleMaxValue, "Compare scale max value after deploying contract");
-        AssertAddress.equal(evaluation.beneficiary(), expectedBeneficiary, "Compare beneficiary address after deploying contract");
+        AssertAddress.equal(evaluation.beneficiary(), beneficiary, "Compare beneficiary address after deploying contract");
         AssertString.equal(evaluation.beneficiaryName(), expectedBeneficiaryName, "Compare beneficiary name after deploying contract");
 
-        address firstJury = 0xD0D1597614662cf53C1ADA223D9268b984B68714;
-        address secondJury = 0x2c61097258C54cE52143224a5169cA082A6c7203;
-        address thirdJury = 0x5C9b7ce8b884f5D988578d3B58DcBCF8Fa15F758;
         address fakeJury = 0x67fDea9dFACc29a20aef5Cd0B833B7d0485AeDfd;
         AssertBool.isTrue(evaluation.juries(firstJury), "Check if jury address is marked as true after deploying contract");
         AssertBool.isTrue(evaluation.juries(secondJury), "Check if jury address is marked as true after deploying contract");
@@ -43,7 +45,6 @@ contract TestEvaluation {
         string memory secret = "s3cr37";
 
         bytes32 evaluationHash = keccak256(abi.encodePacked(value, fake, secret));
-        uint evaluationSumBeforeEvaluate = evaluation.evaluationSum();
 
         payable(address(evaluation)).transfer(10);
         evaluation.evaluate(evaluationHash);
@@ -52,10 +53,45 @@ contract TestEvaluation {
     }
 
     function testReveal() public {
+        if (!evaluation.isHappened(evaluation.evaluationEnd()) ||
+            evaluation.isHappened(evaluation.revealEnd()) ||
+            evaluation.evaluatorsRevealed(msg.sender)
+        ) {
+            return;
+        }
 
+        AssertBool.isFalse(evaluation.evaluatorsRevealed(msg.sender), "Should be marked as false before reveal");
+        uint evaluationSumBeforeReveal = evaluation.evaluationSum();
+
+        uint value = 9;
+        bool fake = false;
+        string memory secret = "s3cr37";
+        evaluation.reveal(value, fake, secret);
+
+        uint evaluationSumAfterReveal = evaluation.evaluationSum();
+
+        AssertUint.isAbove(evaluationSumAfterReveal, evaluationSumBeforeReveal, "After reveal evaluation sum should be greater than until");
+        AssertBool.isTrue(evaluation.evaluatorsRevealed(msg.sender), "Should be marked as true after reveal");
     }
 
     function testEndEvaluation() public {
+        if (!evaluation.isHappened(evaluation.revealEnd()) ||
+            evaluation.evaluationEnded()) {
+            return;
+        }
 
+        evaluation.endEvaluation();
+
+        AssertBool.isTrue(evaluation.evaluationEnded(), "After evaluation ending that variable should be true");
+
+        uint expectedDepositAfterRefund = 0;
+
+        (bytes32 firstJuryEvaluation, uint firstJuryDeposit) = evaluation.evaluations(firstJury);
+        (bytes32 secondJuryEvaluation, uint secondJuryDeposit) = evaluation.evaluations(secondJury);
+        (bytes32 thirdJuryEvaluation, uint thirdJuryDeposit) = evaluation.evaluations(thirdJury);
+
+        AssertUint.equal(firstJuryDeposit, expectedDepositAfterRefund, "Must be reset to zero");
+        AssertUint.equal(secondJuryDeposit, expectedDepositAfterRefund, "Must be reset to zero");
+        AssertUint.equal(thirdJuryDeposit, expectedDepositAfterRefund, "Must be reset to zero");
     }
 }
